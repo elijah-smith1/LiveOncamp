4dd8658cbebac36ebc124ef5b59c21872c5c22fd
Hackerman69420 <mikedccs06@gmail.com>
2024-04-14 17:43:59 -0400

    ui update

---
+++ OnCampApp/MainViews/Social/PartyEvents.swift
@@ -0,0 +1,18 @@
+//
+//  PartyEvents.swift
+//  OnCampApp
+//
+//  Created by Michael Washington on 3/28/24.
+//
+
+import SwiftUI
+
+struct PartyEvents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartyEvents()
+}
---
+++ OnCampApp/MainViews/Social/SchoolEvents.swift
@@ -0,0 +1,18 @@
+//
+//  SchoolEvents.swift
+//  OnCampApp
+//
+//  Created by Michael Washington on 3/28/24.
+//
+
+import SwiftUI
+
+struct SchoolEvents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SchoolEvents()
+}
