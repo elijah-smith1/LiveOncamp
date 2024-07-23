ab1b7f0c53fa19a43fbc4b599c8395a4c674908a
Hackerman69420lol <133720792+Hackerman69420lol@users.noreply.github.com>
2024-07-22 20:20:08 -0400

    commi

---
+++ OnCampApp/Components/GroupBoxComponents.swift
@@ -0,0 +1,8 @@
+//
+//  GroupBoxComponents.swift
+//  OnCampApp
+//
+//  Created by Michael Washington on 6/30/24.
+//
+
+import Foundation
---
+++ OnCampApp/Components/TicketConfimation.swift
@@ -0,0 +1,18 @@
+//
+//  TicketConfimation.swift
+//  OnCampApp
+//
+//  Created by Michael Washington on 7/21/24.
+//
+
+import SwiftUI
+
+struct TicketConfimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TicketConfimation()
+}
